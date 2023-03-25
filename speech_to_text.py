aad6dae3121e26e791c80d30262880e19a9891ef
Erik Goughnour <e.goughnour@gmail.com>
2023-03-25 17:00:57 -0500

    writing to timestamps.json

--- speech_to_text.py
+++ speech_to_text.py
@@ -2,6 +2,9 @@ import whisper
 import os
 import numpy as np
 import torch
+from string import punctuation
+import json
+from pathlib import Path
 
 torch.cuda.is_available()
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
@@ -21,8 +24,21 @@ print(result.text)
 result = model.transcribe("steam_ham_audio.wav", word_timestamps=True)
 print(result["text"])
 segment_list = [segment for segment in result['segments']]
+segment_dict = {'segments':{}}
 for i, segment in enumerate(segment_list):
     start, end, whole_text, words =  (segment['start'], segment['end'], segment['text'],segment['words'])
-    #TODO save to output with file.write(json.dumps())?
+    segment_data = {'start':start, 'end':end, 'text':whole_text, 'words':{}}
     for j, word in enumerate(words):
         print(f"""{j:02d}. {word['word']} [{word['start']}-{word['end']}]({word['probability']:.5f}%) """)
+        word_data = {'word':word['word'], 'bare_word':word['word'].strip(punctuation).lower(), 'start':word['start'], 'end':word['end'], 'probability':word['probability']}
+        segment_data['words'][j] = word_data
+    segment_dict['segments'][i] = segment_data
+json_value = json.dumps(segment_dict, ensure_ascii=False, )
+output_file = 'timestamps.json'
+with open(output_file, 'w') as f:
+    f.write(json_value)
+output_path = Path(output_file).resolve()
+if not (output_path.exists() and output_path.if_file()):
+    print(f'file {output_path} was not written')
+else:
+    print(f'audio script with timestamps has been saved to: {output_path}')
