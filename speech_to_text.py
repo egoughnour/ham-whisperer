7c6bdc0d4d69dcb4fe7e03acd40b132209330b91
Erik Goughnour <e.goughnour@gmail.com>
2023-03-25 17:43:13 -0500

    changed audio in

--- speech_to_text.py
+++ speech_to_text.py
@@ -13,7 +13,7 @@ print(
     f"Model is {'multilingual' if model.is_multilingual else 'English-only'} "
     f"and has {sum(np.prod(p.shape) for p in model.parameters()):,} parameters."
 )
-audio = whisper.load_audio("steam_ham_audio.wav")
+audio = whisper.load_audio("seymour_audio.wav")
 audio = whisper.pad_or_trim(audio)
 mel = whisper.log_mel_spectrogram(audio).to(model.device)
 _, probs = model.detect_language(mel)
@@ -21,7 +21,7 @@ print(f"Detected language: {max(probs, key=probs.get)}")
 options = whisper.DecodingOptions(language="en", without_timestamps=True, fp16=False)
 result = whisper.decode(model, mel, options)
 print(result.text)
-result = model.transcribe("steam_ham_audio.wav", word_timestamps=True)
+result = model.transcribe("seymour_audio.wav", word_timestamps=True)
 print(result["text"])
 segment_list = [segment for segment in result['segments']]
 segment_dict = {'segments':{}}
