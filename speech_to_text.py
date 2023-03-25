bd4937ab82bf7985e332c8d17f5b53658ad5bebe
Erik Goughnour <e.goughnour@gmail.com>
2023-03-25 15:16:26 -0500

    update to medium model

--- speech_to_text.py
+++ speech_to_text.py
@@ -5,7 +5,7 @@ import torch
 
 torch.cuda.is_available()
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-model = whisper.load_model("small", device=DEVICE)
+model = whisper.load_model("medium", device=DEVICE)
 print(
     f"Model is {'multilingual' if model.is_multilingual else 'English-only'} "
     f"and has {sum(np.prod(p.shape) for p in model.parameters()):,} parameters."
