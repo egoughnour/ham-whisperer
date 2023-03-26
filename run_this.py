f223c81efd1a6635558463afc35bcaefef6943e8
Erik Goughnour <e.goughnour@gmail.com>
2023-03-25 21:09:29 -0500

    updated matching to function. updated runner to call it. avoids re-running on existing match data

--- run_this.py
+++ run_this.py
@@ -1,6 +1,17 @@
 import speech_to_text
+import start_compose
+import pathlib
+
+
+def is_existing_json(path: str) -> bool:
+    path_to_check = pathlib.Path(path).resolve()
+    return path_to_check.exists() and path_to_check.is_file()
 
 input_audio='full_steam.wav'
 output_path='full_ham.json'
+search_path='lsoh_sseymour_timestamps.json'
 listen_for = ['Albany', 'Utica', 'Aurora borealis', 'Seymour', 'Superintendent', 'Chalmers']
-speech_to_text.transcribe_to_json(output_path=output_path, input_path=input_audio, suggested_phrases=listen_for)
+if not is_existing_json(output_path):
+    speech_to_text.transcribe_to_json(output_path=output_path, input_path=input_audio, suggested_phrases=listen_for)
+match_count, matches = start_compose.get_matches_from_json(output_path, search_path)
+print(f'Match count is: {match_count}')
--- start_compose.py
+++ start_compose.py
@@ -1,6 +1,7 @@
 import json
 import numpy as np
 from dataclasses import dataclass
+from typing import List, Tuple
 
 @dataclass
 class WordMatch:
@@ -10,40 +11,43 @@ class WordMatch:
     target: dict
     candidate: dict
 
-matches = []
-with open('target_timestamps.json') as f:
-    to_match = json.load(f)
-with open('lsoh_sseymour_timestamps.json') as f:
-    to_search = json.load(f)
-print('loaded')
-search_segs = to_search['segments']
-target_segs = to_match['segments']
+def get_matches_from_json(file_to_match:str, file_to_search:str) -> Tuple[int, List[WordMatch]]:
+    matches = []
+    with open(file_to_match) as f:
+        to_match = json.load(f)
+    with open(file_to_search) as f:
+        to_search = json.load(f)
+    print('loaded')
+    search_segs = to_search['segments']
+    target_segs = to_match['segments']
 
-#don't need to enumerate because the keys are indexes
-for i, wanted_seg in target_segs.items():
-    for j, found_seg in search_segs.items():
-        i = int(i)
-        j = int(j)
-        words_needed = [ (s['bare_word'].strip(), s) for _,s in wanted_seg['words'].items()]
-        words_available = [(s['bare_word'].strip(), s) for _,s in found_seg['words'].items()]
-        #TODO normalize probabilities?
-        for needed, n_data in  words_needed:
-            target_time = n_data['end'] - n_data['start']
-            pt = n_data['probability']
-            for available, a_data in words_available:
-                if needed == available:
-                    candidate_time = a_data['end']-a_data['start']
-                    pc = a_data['probability']
-                    relative_time_mismatch = np.abs(1.0 - (candidate_time/target_time))
-                    reliability = pt * pc
-                    matches.append(WordMatch(needed, relative_time_mismatch, reliability, n_data, a_data))
-unique_matches = set()
-for match in matches:
-    unique_matches.add(match.match_text)
+    #don't need to enumerate because the keys are indexes
+    for i, wanted_seg in target_segs.items():
+        for j, found_seg in search_segs.items():
+            i = int(i)
+            j = int(j)
+            words_needed = [ (s['bare_word'].strip(), s) for _,s in wanted_seg['words'].items()]
+            words_available = [(s['bare_word'].strip(), s) for _,s in found_seg['words'].items()]
+            #TODO normalize probabilities?
+            for needed, n_data in  words_needed:
+                target_time = n_data['end'] - n_data['start']
+                pt = n_data['probability']
+                for available, a_data in words_available:
+                    if needed == available:
+                        candidate_time = a_data['end']-a_data['start']
+                        pc = a_data['probability']
+                        relative_time_mismatch = np.abs(1.0 - (candidate_time/target_time)) if ((target_time > 0) and (candidate_time > 0))  else -1
+                        reliability = pt * pc
+                        matches.append(WordMatch(needed, relative_time_mismatch, reliability, n_data, a_data))
+    unique_matches = set()
+    for match in matches:
+        unique_matches.add(match.match_text)
 
-match_count = len(unique_matches)
+    match_count = len(unique_matches)
 
-print(f"""Found {match_count} matches""")
+    print(f"""Found {match_count} matches""")
+    return match_count, matches
+
 
 
 
