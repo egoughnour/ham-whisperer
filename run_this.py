b4a56a194810925bc0969f2fa3eb3c00e76e997f
Erik Goughnour <e.goughnour@gmail.com>
2023-03-30 21:51:41 -0500

    added moviepy (#16)
    added moviepy, updated requirements.
    changed arg parsing
    modified conditions in process()
    added additional function parameters
    add clip_index attribute to WordMatch type
    Now reading, clipping, matching, splicing
    Should satisfy issues #2, #3, #13, #15
    THE MAJOR CAVEAT is that video splicing is not yet implemented

--- run_this.py
+++ run_this.py
@@ -17,7 +17,8 @@ def read_prompt_file(prompt_path: Path) -> List[str]:
     with open(prompt_path) as f:
         return f.readlines()
 
-def process(input_audio, output_path, search_path, prompt_file, whisper_model):
+def process(input_audio, output_path, search_path, prompt_file, whisper_model, other_audio_file):
+    match_count = None
     if prompt_file:
         listen_for = read_prompt_file(prompt_path=prompt_file)
     else:
@@ -29,16 +30,19 @@ def process(input_audio, output_path, search_path, prompt_file, whisper_model):
     if not is_existing_json(output_path):
         speech_to_text.transcribe_to_json(output_path=(output_path_string := str(output_path.resolve())), input_path=str(input_audio.resolve()), suggested_phrases=listen_for, whisper_model=whisper_model)
     if search_path:
-        match_count, matches = start_compose.get_matches_from_json(output_path_string, str(search_path.resolve()))
-    print(f'Match count is: {match_count}')
+        output_path_string = str(output_path.resolve())
+        match_count, matches = start_compose.get_matches_from_json(output_path_string, str(search_path.resolve()), str(input_audio.resolve()), str(other_audio_file.resolve()) if other_audio_file is not None else None)
+    if match_count is not None:
+        print(f'Match count is: {match_count}')
 
 def main():
     parser = ArgumentParser()
     parser.add_argument('input_audio', type=Path, help='path to an audio file that will be transcribed')
     parser.add_argument('output_path', type=Path, help='path to a JSON file to store the transcription data.  If it exists, transcription is assumed complete.')
-    parser.add_argument('search_path', type=Path, default=None, help='path to another JSON file. This file contains the transcription data for an audio or video source to search, matching against the first (word by word).')
+    parser.add_argument('search_path', nargs='?',type=Path, default=None, help='path to another JSON file. This file contains the transcription data for an audio or video source to search, matching against the first (word by word).')
     parser.add_argument('-p','--prompt-file', type=Path, help='the path to a file containing hints for difficult to decode words or phrases.  The phrases or word should simply be listed one per line without punctuation.')
     parser.add_argument('-w','--whisper-model', type=str, default='medium', help='The model to use for whisper.')
+    parser.add_argument('-o', '--other-audio-file', type=Path, default=None, help='the path to an other audio file. Namely the audio file corresponding to the source of the JSON file on the search path.')
     args = parser.parse_args()
     process(**vars(args))
 
--- start_compose.py
+++ start_compose.py
@@ -2,6 +2,8 @@ import json
 import numpy as np
 from dataclasses import dataclass
 from typing import List, Tuple
+from moviepy import *
+from moviepy.editor import *
 
 @dataclass
 class WordMatch:
@@ -10,8 +12,9 @@ class WordMatch:
     detected_word_reliability: float
     target: dict
     candidate: dict
+    clip_index: int
 
-def get_matches_from_json(file_to_match:str, file_to_search:str) -> Tuple[int, List[WordMatch]]:
+def get_matches_from_json(file_to_match:str, file_to_search:str, input_audio_file:str, other_audio_file:str) -> Tuple[int, List[WordMatch]]:
     matches = []
     with open(file_to_match) as f:
         to_match = json.load(f)
@@ -38,16 +41,60 @@ def get_matches_from_json(file_to_match:str, file_to_search:str) -> Tuple[int, L
                         pc = a_data['probability']
                         relative_time_mismatch = np.abs(1.0 - (candidate_time/target_time)) if ((target_time > 0) and (candidate_time > 0))  else -1
                         reliability = pt * pc
-                        matches.append(WordMatch(needed, relative_time_mismatch, reliability, n_data, a_data))
+                        matches.append(WordMatch(needed, relative_time_mismatch, reliability, n_data, a_data, -1))
     unique_matches = set()
     for match in matches:
         unique_matches.add(match.match_text)
 
     match_count = len(unique_matches)
 
+    best_matches = []
+    for unique_text in unique_matches:
+        related_matches = [m for m in  matches if m.match_text == unique_text]
+        best_match = max(related_matches, key=lambda x: x.detected_word_reliability)
+        best_matches.append(best_match)
+
+    sub_clips = []
+   
+    if  other_audio_file is not None:
+        original_clip = AudioFileClip(input_audio_file)
+        clip = AudioFileClip(other_audio_file)
+
+        for i, bm in enumerate(best_matches):
+            sub_clip = clip.subclip(bm.candidate["start"], bm.candidate["end"])
+            sub_clips.append(sub_clip)
+            bm.clip_index = len(sub_clips)-1
+
+        clips_to_concat = []
+        hits = 0
+        misses = 0
+        matches_for_clipword = [fw.target for fw in best_matches]
+        for i, seg in target_segs.items():
+            target_time = seg['end'] - seg['start']
+            if (target_time == 0) or (not seg['text']) or (not seg['words']):
+                continue
+            for j, clip_word in seg['words'].items():
+                if clip_word in matches_for_clipword:
+                    hits += 1    
+                    word_match_index = matches_for_clipword.index(clip_word)
+                    match_for_this_word = best_matches[word_match_index]
+                    clips_to_concat.append(sub_clips[match_for_this_word.clip_index].copy())
+                else:
+                    misses += 1
+                    clips_to_concat.append(original_clip.subclip(clip_word['start'], clip_word['end']))
+        #TODO fix condition here assert(len(clips_to_concat) == len())
+
+        print(f'misses: {misses}')
+        print(f'hits: {hits}')
+
+        patchwork = concatenate_audioclips(clips_to_concat) 
+        patchwork.write_audiofile('masterpiece.wav')
+
+
+
+
     print(f"""Found {match_count} matches""")
     return match_count, matches
-
 
 
 
